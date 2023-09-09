98dda8f54ddbd19f9d2fdb24fa1128311bcc5a64
Gustavo Daniel Britez Alvarenga <115171270+GustavoBritez@users.noreply.github.com>
2023-09-09 01:43:35 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Ejercicio2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_Comenzar = new System.Windows.Forms.Button();
+            this.TX_1 = new System.Windows.Forms.TextBox();
+            this.LT_1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BTN_Comenzar
+            // 
+            this.BTN_Comenzar.Location = new System.Drawing.Point(204, 83);
+            this.BTN_Comenzar.Name = "BTN_Comenzar";
+            this.BTN_Comenzar.Size = new System.Drawing.Size(75, 23);
+            this.BTN_Comenzar.TabIndex = 0;
+            this.BTN_Comenzar.Text = "button1";
+            this.BTN_Comenzar.UseVisualStyleBackColor = true;
+            this.BTN_Comenzar.Click += new System.EventHandler(this.BTN_Comenzar_Click);
+            // 
+            // TX_1
+            // 
+            this.TX_1.Location = new System.Drawing.Point(137, 33);
+            this.TX_1.Name = "TX_1";
+            this.TX_1.Size = new System.Drawing.Size(225, 20);
+            this.TX_1.TabIndex = 1;
+            // 
+            // LT_1
+            // 
+            this.LT_1.FormattingEnabled = true;
+            this.LT_1.Location = new System.Drawing.Point(187, 135);
+            this.LT_1.Name = "LT_1";
+            this.LT_1.Size = new System.Drawing.Size(120, 95);
+            this.LT_1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 242);
+            this.Controls.Add(this.LT_1);
+            this.Controls.Add(this.TX_1);
+            this.Controls.Add(this.BTN_Comenzar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_Comenzar;
+        private System.Windows.Forms.TextBox TX_1;
+        private System.Windows.Forms.ListBox LT_1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    public partial class Form1 : Form
+    {
+        List<int> Caja = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// Escribir una función recursiva que devuelva los primeros N númerospares
+        private int Pares(int num, List<int> lista)
+        {
+            if( num > 1 )
+            {
+                if( num % 2 == 0)
+                {
+                    lista.Add(num);
+                    return Pares(num - 2, lista);
+                }
+                else
+                {
+                    return Pares(num - 1, lista);
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private void BTN_Comenzar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TX_1.Text))
+            {
+                if (Int32.TryParse(TX_1.Text, out int num))
+                {
+
+                    Pares(num, Caja);
+
+                    // Agrega los números pares a la lista LT_1
+                    foreach (int par in Caja)
+                    {
+                        LT_1.Items.Add(par.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
